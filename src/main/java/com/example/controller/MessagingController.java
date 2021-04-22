b577ee1243b0c487ba4fbc4d5870e7dfb58ea6ff
Gian Eric <gianeric08@gmail.com>
2021-04-21 23:31:24 -0300

    [App] Consumindo pedidos da fila

--- src/main/java/com/example/controller/MessagingController.java
+++ src/main/java/com/example/controller/MessagingController.java
@@ -36,4 +36,23 @@ public class MessagingController {
             return " [x] Sent. '" + message + "'";
         }
     }
+
+    @GetMapping
+    public String Consumer() throws Exception{
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            System.out.println("[*] Waiting for messages.");
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+
+            };
+            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+            return " [x] Received '" + message + "'";
+        }
+    }
 }
