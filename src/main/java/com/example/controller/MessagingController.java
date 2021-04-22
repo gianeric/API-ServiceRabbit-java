6ccf189abf754c0b431d657efe30cbb05c92a7e0
Gian Eric <gianeric08@gmail.com>
2021-04-21 23:22:56 -0300

    [App] Produzindo pedidos na fila

--- src/main/java/com/example/controller/MessagingController.java
+++ src/main/java/com/example/controller/MessagingController.java
@@ -7,21 +7,31 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/amqplib/callback_api")
 public class MessagingController {
-    private final static String QUEUE_NAME = "hello";
-    private String message;
+    private final static String QUEUE_NAME = "pedidos_spring";
+    private String message = "";
 
     @PostMapping
-    public String Producer() throws Exception{
+    public static String Producer() throws Exception{
+        String message;
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            message = "Hello World!";
+            message =  "{"                                                       +
+                       "data: 01/01/2021,"                                       +
+                       "nome: Gian Eric,"                                        +
+                       "servicos: [{descricao: Instalação do motor de arranque},"+
+                       "{descricao: Instalação do amortecedor},"                 +
+                       "{descricao: Troca da bomba de gasolina}]"                +
+                       "}";
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
             return " [x] Sent. '" + message + "'";
         }
