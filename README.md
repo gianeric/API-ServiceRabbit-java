ee9f5a1b2b26d3ba3dfaa223bc698353f774213b
Gian Eric <gianeric08@gmail.com>
2021-05-03 03:27:12 -0300

    [Readme] Add gifs de producer e consumer com RabbitMQ e reorganização do código no controller

--- README.md
+++ README.md
@@ -12,4 +12,26 @@ Result
 >| ----------- | ------------------------- | ----------------------- | ------------------- | -------------------| ------ | ------------------------------------------------------------------------------------------------------------- | -------- |
 >| 4f56218f00ff|   rabbitmq:3-management   | "docker-entrypoint.s…"  | About an hour ago   | Up About an hour   |        | 4369/tcp, 5671/tcp, 0.0.0.0:5672->5672/tcp, 15671/tcp, 15691-15692/tcp, 25672/tcp, 0.0.0.0:15672->15672/tcp   | rabbitmq |
 
+Open RabbitMQ
+> <link>localhost:15672</link>
+
+Running the Spring Boot application
+<p align="center">
+  <img src="https://imgur.com/4xeVTJb.gif" width="600" title="Screenshot">
+</p>
+
+Producing a message in the queue:
+
+Note: The queue consumed is the same as for the <a href="https://github.com/gianeric/API-ServiceRabbit-nodejs">API-ServiceRabbit-nodejs</a>, we named this queue with the name "pedidos_node"
+<p align="center">
+  <img src="https://imgur.com/pxDI2Tq.gif" width="600" title="Screenshot">
+</p>
+
+Consuming messages from the queue:
+
+Note: The queue consumed is the same as for the <a href="https://github.com/gianeric/API-ServiceRabbit-nodejs">API-ServiceRabbit-nodejs</a>, we named this queue with the name "pedidos_node"
+<p align="center">
+  <img src="https://imgur.com/kOmv851.gif" width="600" title="Screenshot">
+</p>
+
 
--- src/main/java/com/example/controller/MessagingController.java
+++ src/main/java/com/example/controller/MessagingController.java
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit;
 @RestController
 @RequestMapping("/amqplib/callback_api")
 public class MessagingController {
-    private final static String QUEUE_NAME = "pedidos_spring";
+    private final static String QUEUE_NAME = "pedidos_node";
     private String message = "";
 
     @PostMapping
@@ -24,7 +24,7 @@ public class MessagingController {
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
             message =  "{"                                                       +
                        "data: 01/01/2021,"                                       +
                        "nome: Gian Eric,"                                        +
@@ -41,18 +41,23 @@ public class MessagingController {
     public String Consumer() throws Exception{
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            System.out.println("[*] Waiting for messages.");
 
-            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-                message = new String(delivery.getBody(), "UTF-8");
-                System.out.println(" [x] Received '" + message + "'");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
 
-            };
-            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
-            return " [x] Received '" + message + "'";
-        }
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        System.out.println("[*] Waiting for messages.");
+
+        channel.basicQos(1);
+
+        //Wait messages
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            message = new String(delivery.getBody(), "UTF-8");
+
+            System.out.println(" [x] Received '" + message + "'");
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+        return " [x] Received '" + message + "'";
     }
 }
